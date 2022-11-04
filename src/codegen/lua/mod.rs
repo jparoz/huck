498c5e5ee6243034e20cd44e67ec48c3591369d4
Jesse Adamson Paroz <jesse.paroz@gmail.com>
2022-11-04 16:04:12 +1000

    Some more progress on codegen

--- src/codegen/lua/mod.rs
+++ src/codegen/lua/mod.rs
@@ -1,5 +1,7 @@
-use crate::ast::Assignment;
+use crate::ast;
 use crate::scope::Scope;
+
+const HUCK_TABLE_NAME: &str = "Huck";
 
 pub trait Generate {
     fn generate(&self) -> String;
@@ -12,7 +14,6 @@ impl<'file> Generate for Scope<'file> {
     fn generate(&self) -> String {
         let mut lua = "return {".to_string();
 
-        // lua.push_str("")
         for (name, defn) in self.iter() {
             lua.push_str(&format!("[\"{}\"] = ", name));
 
@@ -33,10 +34,112 @@ impl<'file> Generate for Scope<'file> {
     }
 }
 
-impl<'file> Generate for Assignment<'file> {
+impl<'file> Generate for Vec<ast::Assignment<'file>> {
     fn generate(&self) -> String {
-        let lua = String::new();
+        todo!()
+    }
+}
+
+impl<'file> Generate for ast::Assignment<'file> {
+    fn generate(&self) -> String {
+        let mut lua = String::new();
+
         let (lhs, expr) = self;
+
+        match lhs {
+            ast::Lhs::Func { name, args } if args.is_empty() => {
+                // It's a value
+                lua.push_str(&expr.generate());
+            }
+            ast::Lhs::Func { name, args } => {
+                // It's a function
+                todo!()
+            }
+            ast::Lhs::Binop { a, op, b } => {
+                // It's a function
+                todo!()
+            }
+        }
+
+        lua
+    }
+}
+
+impl<'file> Generate for ast::Expr<'file> {
+    fn generate(&self) -> String {
+        match self {
+            ast::Expr::Term(term) => term.generate(),
+            ast::Expr::App { func, argument } => {
+                let mut lua = String::new();
+
+                // Func (as an expr, surrounded in brackets)
+                lua.push('('); // @Note: not strictly necessary?
+                lua.push_str(&func.generate());
+                lua.push(')'); // @Note: not strictly necessary?
+
+                // Argument (function call syntax)
+                // @Note: this assumes functions are curried (? maybe not if tuples desugar to
+                // bare (func)(a, b) instead of (func)((a, b)) which doesn't make sense anyway)
+                lua.push('(');
+                lua.push_str(&argument.generate());
+                lua.push(')');
+
+                lua
+            }
+            ast::Expr::Binop { operator, lhs, rhs } => {
+                let mut lua = String::new();
+
+                // @Note @Fixme: this should check for Lua's builtin operators and use those
+                // if available
+
+                // Op
+                lua.push_str(&format!("{}[\"{}\"]", HUCK_TABLE_NAME, operator));
+
+                // Argument (function call syntax)
+                // @Note: this assumes functions are curried (? maybe not if tuples desugar to
+                // bare (func)(a, b) instead of (func)((a, b)) which doesn't make sense anyway)
+                lua.push('(');
+                lua.push_str(&lhs.generate());
+                lua.push_str(", ");
+                lua.push_str(&rhs.generate());
+                lua.push(')');
+
+                lua
+            }
+            ast::Expr::Let {
+                assignments,
+                in_expr,
+            } => {
+                let mut lua = String::new();
+
+                // Open a new function, and local variable declaration
+                lua.push_str("(function()\n");
+
+                // Make a new local variable for each assignment
+                for assign in assignments.values() {
+                    lua.push_str("local ");
+                    lua.push_str(&assign.generate()); // @XXX @Fixme: This is certainly wrong
+                    lua.push('\n');
+                }
+
+                // Generate the in_expr
+                lua.push_str("return ");
+                lua.push_str(&in_expr.generate());
+                lua.push('\n');
+
+                lua.push_str("end)()");
+
+                lua
+            }
+            ast::Expr::Lambda { args, rhs } => {
+                todo!();
+            }
+        }
+    }
+}
+
+impl<'file> Generate for ast::Term<'file> {
+    fn generate(&self) -> String {
         todo!()
     }
 }
