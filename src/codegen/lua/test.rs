013157fe90f4bd16a2e240f22217ce6967bd62ed
Jesse Adamson Paroz <jesse.paroz@gmail.com>
2022-11-22 16:37:48 +1000

    Fix tests

--- src/codegen/lua/test.rs
+++ src/codegen/lua/test.rs
@@ -1,15 +1,5 @@
 use crate::utils::{normalize, transpile};
 
-macro_rules! wrap_lua {
-    ($lua:expr) => {{
-        let mut lua = String::new();
-        lua.push_str("local M = {}\n\n");
-        lua.push_str(&$lua);
-        lua.push_str("\n\nreturn M");
-        lua
-    }};
-}
-
 #[test]
 fn lambda_equals_function() {
     assert_eq!(
@@ -22,17 +12,19 @@ fn lambda_equals_function() {
 fn function_const() {
     assert_eq!(
         transpile(r#"const a b = a;"#).unwrap(),
-        normalize(&wrap_lua!(
+        normalize(
             r#"
-                M["const"] = function(_HUCK_0)
+                local _HUCK = {}
+                _HUCK["const"] = function(_HUCK_0)
                     return function(_HUCK_1)
                         local a = _HUCK_0
                         local b = _HUCK_1
                         return a
                     end
                 end
+                return {["const"] = _HUCK["const"]}
             "#
-        ))
+        )
     )
 }
 
@@ -40,14 +32,16 @@ fn function_const() {
 fn function_id() {
     assert_eq!(
         transpile(r#"id a = a;"#).unwrap(),
-        normalize(&wrap_lua!(
+        normalize(
             r#"
-                M["id"] = function(_HUCK_0)
+                local _HUCK = {}
+                _HUCK["id"] = function(_HUCK_0)
                     local a = _HUCK_0
                     return a
                 end
+                return {["id"] = _HUCK["id"]}
             "#
-        ))
+        )
     )
 }
 
@@ -55,9 +49,10 @@ fn function_id() {
 fn function_not() {
     assert_eq!(
         transpile(r#"not True = False; not False = True;"#).unwrap(),
-        normalize(&wrap_lua!(
+        normalize(
             r#"
-                M["not"] = function(_HUCK_0)
+                local _HUCK = {}
+                _HUCK["not"] = function(_HUCK_0)
                     if (getmetatable(_HUCK_0).__variant == "True") then
                         return False
                     end
@@ -66,8 +61,9 @@ fn function_not() {
                     end
                     error("Unmatched pattern in function `not`")
                 end
+                return {["not"] = _HUCK["not"]}
             "#
-        ))
+        )
     )
 }
 
@@ -75,9 +71,10 @@ fn function_not() {
 fn function_and() {
     assert_eq!(
         transpile(r#"True && True = True; _ && _ = False;"#).unwrap(),
-        normalize(&wrap_lua!(
+        normalize(
             r#"
-                M["&&"] = function(_HUCK_0)
+                local _HUCK = {}
+                _HUCK["&&"] = function(_HUCK_0)
                     return function(_HUCK_1)
                         if (getmetatable(_HUCK_0).__variant == "True") and
                             (getmetatable(_HUCK_1).__variant == "True") then
@@ -88,7 +85,8 @@ fn function_and() {
                             return False
                     end
                 end
+                return {["&&"] = _HUCK["&&"]}
             "#
-        ))
+        )
     )
 }
