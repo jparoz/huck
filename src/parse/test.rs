cd535ba7d28317085cdc9ef27c2c5a962e5ddf2a
Jesse Adamson Paroz <jesse.paroz@gmail.com>
2022-11-25 13:26:45 +1000

    Reformat tests

--- src/parse/test.rs
+++ src/parse/test.rs
@@ -12,60 +12,44 @@ fn binop_plus() {
 
 #[test]
 fn unit() {
-    assert_eq!(
-        parse(
-            r#"
-                unit = ();
-            "#
-        )
-        .unwrap(),
-        {
-            let mut definitions = HashMap::new();
+    assert_eq!(parse(r#"unit = ();"#).unwrap(), {
+        let mut definitions = HashMap::new();
 
-            let name = ast::Name::Ident("unit".to_string());
-            definitions.insert(
-                name.clone(),
-                vec![(
-                    ast::Lhs::Func { name, args: vec![] },
-                    ast::Expr::Term(ast::Term::Unit),
-                )],
-            );
+        let name = ast::Name::Ident("unit".to_string());
+        definitions.insert(
+            name.clone(),
+            vec![(
+                ast::Lhs::Func { name, args: vec![] },
+                ast::Expr::Term(ast::Term::Unit),
+            )],
+        );
 
-            ast::Chunk::new(definitions)
-        }
-    )
+        ast::Chunk::new(definitions)
+    })
 }
 
 #[test]
 fn apply_to_unit() {
-    assert_eq!(
-        parse(
-            r#"
-                applyToUnit f = f ();
-            "#
-        )
-        .unwrap(),
-        {
-            let mut definitions = HashMap::new();
+    assert_eq!(parse(r#"applyToUnit f = f ();"#).unwrap(), {
+        let mut definitions = HashMap::new();
 
-            let name = ast::Name::Ident("applyToUnit".to_string());
-            definitions.insert(
-                name.clone(),
-                vec![(
-                    ast::Lhs::Func {
-                        name,
-                        args: vec![ast::Pattern::Bind("f")],
-                    },
-                    ast::Expr::App {
-                        func: Box::new(ast::Expr::Term(ast::Term::Name(ast::Name::Ident(
-                            "f".to_string(),
-                        )))),
-                        argument: Box::new(ast::Expr::Term(ast::Term::Unit)),
-                    },
-                )],
-            );
+        let name = ast::Name::Ident("applyToUnit".to_string());
+        definitions.insert(
+            name.clone(),
+            vec![(
+                ast::Lhs::Func {
+                    name,
+                    args: vec![ast::Pattern::Bind("f")],
+                },
+                ast::Expr::App {
+                    func: Box::new(ast::Expr::Term(ast::Term::Name(ast::Name::Ident(
+                        "f".to_string(),
+                    )))),
+                    argument: Box::new(ast::Expr::Term(ast::Term::Unit)),
+                },
+            )],
+        );
 
-            ast::Chunk::new(definitions)
-        }
-    )
+        ast::Chunk::new(definitions)
+    })
 }
