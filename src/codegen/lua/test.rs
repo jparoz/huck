67065790ea69901acae725dd74554fa83e1c1345
Jesse Adamson Paroz <jesse.paroz@gmail.com>
2022-11-29 16:33:32 +1000

    Use a wrap! macro to make codegen tests more brief and clearer to read

--- src/codegen/lua/test.rs
+++ src/codegen/lua/test.rs
@@ -1,5 +1,21 @@
 use crate::utils::{normalize, transpile};
 
+macro_rules! wrap {
+    ($name:literal, $expr:literal) => {
+        concat!(
+            "local _HUCK = {}\n_HUCK[\"",
+            $name,
+            "\"] = ",
+            $expr,
+            "\nreturn {[\"",
+            $name,
+            "\"] = _HUCK[\"",
+            $name,
+            "\"]}",
+        )
+    };
+}
+
 #[test]
 fn lambda_equals_function() {
     assert_eq!(
@@ -12,19 +28,18 @@ fn lambda_equals_function() {
 fn function_const() {
     assert_eq!(
         transpile(r#"const a b = a;"#).unwrap(),
-        normalize(
+        normalize(wrap!(
+            "const",
             r#"
-                local _HUCK = {}
-                _HUCK["const"] = function(_HUCK_0)
+                function(_HUCK_0)
                     return function(_HUCK_1)
                         local a = _HUCK_0
                         local b = _HUCK_1
                         return a
                     end
                 end
-                return {["const"] = _HUCK["const"]}
             "#
-        )
+        ))
     )
 }
 
@@ -32,16 +47,15 @@ fn function_const() {
 fn function_id() {
     assert_eq!(
         transpile(r#"id a = a;"#).unwrap(),
-        normalize(
+        normalize(wrap!(
+            "id",
             r#"
-                local _HUCK = {}
-                _HUCK["id"] = function(_HUCK_0)
+                function(_HUCK_0)
                     local a = _HUCK_0
                     return a
                 end
-                return {["id"] = _HUCK["id"]}
             "#
-        )
+        ))
     )
 }
 
@@ -49,10 +63,10 @@ fn function_id() {
 fn function_not() {
     assert_eq!(
         transpile(r#"not True = False; not False = True;"#).unwrap(),
-        normalize(
+        normalize(wrap!(
+            "not",
             r#"
-                local _HUCK = {}
-                _HUCK["not"] = function(_HUCK_0)
+                function(_HUCK_0)
                     if (getmetatable(_HUCK_0).__variant == "True") then
                         return False
                     end
@@ -61,9 +75,8 @@ fn function_not() {
                     end
                     error("Unmatched pattern in function `not`")
                 end
-                return {["not"] = _HUCK["not"]}
             "#
-        )
+        ))
     )
 }
 
@@ -71,10 +84,10 @@ fn function_not() {
 fn function_and() {
     assert_eq!(
         transpile(r#"True && True = True; _ && _ = False;"#).unwrap(),
-        normalize(
+        normalize(wrap!(
+            "&&",
             r#"
-                local _HUCK = {}
-                _HUCK["&&"] = function(_HUCK_0)
+                function(_HUCK_0)
                     return function(_HUCK_1)
                         if (getmetatable(_HUCK_0).__variant == "True") and
                             (getmetatable(_HUCK_1).__variant == "True") then
@@ -85,9 +98,8 @@ fn function_and() {
                             return False
                     end
                 end
-                return {["&&"] = _HUCK["&&"]}
             "#
-        )
+        ))
     )
 }
 
@@ -95,13 +107,7 @@ fn function_and() {
 fn literal_list() {
     assert_eq!(
         transpile(r#"list = [1, 2, 3];"#).unwrap(),
-        normalize(
-            r#"
-                local _HUCK = {}
-                _HUCK["list"] = {1, 2, 3}
-                return {["list"] = _HUCK["list"]}
-            "#
-        )
+        normalize(wrap!("list", "{1, 2, 3}"))
     )
 }
 
@@ -109,13 +115,10 @@ fn literal_list() {
 fn string_escape() {
     assert_eq!(
         transpile(r#"string = "Hello\nworld!\nThis is a quote: `\"`.";"#).unwrap(),
-        normalize(
-            r#"
-                local _HUCK = {}
-                _HUCK["string"] = "Hello\nworld!\nThis is a quote: `\"`."
-                return {["string"] = _HUCK["string"]}
-            "#
-        )
+        normalize(wrap!(
+            "string",
+            r#""Hello\nworld!\nThis is a quote: `\"`.""#
+        ))
     )
 }
 
@@ -123,12 +126,6 @@ fn string_escape() {
 fn tuple() {
     assert_eq!(
         transpile(r#"tuple = (1, "hi");"#).unwrap(),
-        normalize(
-            r#"
-                local _HUCK = {}
-                _HUCK["tuple"] = {1, "hi"}
-                return {["tuple"] = _HUCK["tuple"]}
-            "#
-        )
+        normalize(wrap!("tuple", r#"{1, "hi"}"#))
     )
 }
