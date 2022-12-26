1f46a5908888669f6a819b4d3341980a6454acd1
Jesse Adamson Paroz <jesse.paroz@gmail.com>
2022-12-26 17:56:27 +1000

    Test some more typechecking situations

--- src/types/test.rs
+++ src/types/test.rs
@@ -3,7 +3,7 @@ use crate::parse::parse;
 use super::{
     constraint::{ConstraintGenerator, GenerateConstraints},
     substitution::ApplySub,
-    Type,
+    Type, TypeVar,
 };
 
 fn typ(s: &str) -> Type {
@@ -22,9 +22,93 @@ fn typ(s: &str) -> Type {
 }
 
 #[test]
-fn tuple() {
+fn tuple_is_ordered() {
     let the_typ = typ(r#"a = (1, "hi");"#);
     assert_eq!(the_typ, typ(r#"a = (3, "hello");"#));
     assert_ne!(the_typ, typ(r#"a = ("hello", 3);"#));
     assert_ne!(the_typ, typ(r#"a = (3, "hello", 1);"#));
+}
+
+#[test]
+fn literal_int() {
+    assert_eq!(typ(r#"a = 123;"#), Type::Concrete("Int".to_string()));
+}
+
+#[test]
+fn literal_float() {
+    assert_eq!(typ(r#"a = 1.23;"#), Type::Concrete("Float".to_string()));
+}
+
+#[test]
+fn literal_string() {
+    assert_eq!(
+        typ(r#"a = "Hello, world!";"#),
+        Type::Concrete("String".to_string())
+    );
+}
+
+#[test]
+fn literal_unit() {
+    assert_eq!(typ(r#"a = ();"#), Type::Concrete("()".to_string()));
+}
+
+#[test]
+fn literal_tuple_int_string() {
+    assert_eq!(
+        typ(r#"a = (123, "Hello, world!");"#),
+        Type::Tuple(vec![
+            Type::Concrete("Int".to_string()),
+            Type::Concrete("String".to_string())
+        ])
+    );
+}
+
+#[test]
+fn literal_list_int() {
+    assert_eq!(
+        typ(r#"a = [123, 456];"#),
+        Type::List(Box::new(Type::Concrete("Int".to_string())))
+    );
+}
+
+// @Note @Cleanup: This would be better if we could use matches! to avoid `TypeVar(1)`
+// (could instead be `TypeVar(_)`),
+// but the boxes prevent this.
+// Also true for lots of other tests.
+#[test]
+fn function_id() {
+    assert_eq!(
+        typ(r#"id a = a;"#),
+        Type::Func(
+            Box::new(Type::Var(TypeVar(1))),
+            Box::new(Type::Var(TypeVar(1)))
+        )
+    );
+}
+
+#[test]
+fn function_const() {
+    assert_eq!(
+        typ(r#"const a b = a;"#),
+        Type::Func(
+            Box::new(Type::Var(TypeVar(2))),
+            Box::new(Type::Func(
+                Box::new(Type::Var(TypeVar(1))),
+                Box::new(Type::Var(TypeVar(2)))
+            ))
+        )
+    );
+}
+
+// @Todo: Prelude
+#[ignore]
+#[test]
+fn function_add() {
+    assert_eq!(
+        typ(r#"f x = x + 5;"#),
+        Type::Func(
+            Box::new(Type::Concrete("Int".to_string())),
+            Box::new(Type::Concrete("Int".to_string()))
+        )
+    );
 }
