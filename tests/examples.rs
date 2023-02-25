443b02cf9a676749086cf16d73aa28e3276fcffa
Jesse Adamson Paroz <jesse.paroz@gmail.com>
2023-02-25 23:07:04 +1000

    Test that the example files compile without error
    Closes #5

--- tests/examples.rs
+++ tests/examples.rs
@@ -7,3 +7,17 @@ fn basic() {
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
     cmd.arg("examples/basic.hk").assert().success();
 }
+
+#[test]
+fn foo_and_bar() {
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    cmd.args(["examples/foo.hk", "examples/bar.hk"])
+        .assert()
+        .success();
+}
+
+#[test]
+fn uses_lua_library() {
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    cmd.arg("examples/uses_lua.hk").assert().success();
+}
