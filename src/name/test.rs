bc8e16be95e590ef6b13086c269165b68d89f12e
Jesse Adamson Paroz <jesse.paroz@gmail.com>
2023-03-03 22:05:02 +1000

    Add a few more name resolution tests
    Make name::test naming more consistent with other test modules

--- src/name/test.rs
+++ src/name/test.rs
@@ -7,17 +7,7 @@ use crate::name::Error as NameError;
 use crate::parse::Error as ParseError;
 
 #[test]
-fn name_in_scope() {
-    assert_matches!(transpile("foo = 123; bar = foo;"), Ok(_));
-}
-
-#[test]
-fn let_binding_in_scope() {
-    assert_matches!(transpile("foo = let bar = 123 in bar;"), Ok(_));
-}
-
-#[test]
-fn not_in_scope() {
+fn error_not_in_scope() {
     assert_matches!(
         transpile("foo = 123; bar = baz;"),
         Err(HuckError::NameResolution(NameError::NotInScope(..)))
@@ -25,7 +15,15 @@ fn not_in_scope() {
 }
 
 #[test]
-fn nonexistent_module() {
+fn error_import_clash() {
+    assert_matches!(
+        transpile("import Foo (foo); foo = 123;"),
+        Err(HuckError::NameResolution(NameError::ImportClash(..)))
+    )
+}
+
+#[test]
+fn error_nonexistent_module() {
     assert_matches!(
         transpile("import Foo;"),
         Err(HuckError::NameResolution(NameError::NonexistentModule(..)))
@@ -33,7 +31,7 @@ fn nonexistent_module() {
 }
 
 #[test]
-fn nonexistent_value_name() {
+fn error_nonexistent_value_name() {
     assert_matches!(
         transpile("import Prelude; foo = Prelude.nothing;"),
         Err(HuckError::NameResolution(NameError::NonexistentValueName(
@@ -43,7 +41,7 @@ fn nonexistent_value_name() {
 }
 
 #[test]
-fn nonexistent_type_name() {
+fn error_nonexistent_type_name() {
     assert_matches!(
         transpile("import Prelude; foo : Prelude.Nothing = unsafe lua {};"),
         Err(HuckError::NameResolution(NameError::NonexistentTypeName(
@@ -53,7 +51,17 @@ fn nonexistent_type_name() {
 }
 
 #[test]
-fn nonexistent_name() {
+fn error_nonexistent_constructor_name() {
+    assert_matches!(
+        transpile("import Prelude (Maybe (Just, Nothing, Foo));"),
+        Err(HuckError::NameResolution(
+            NameError::NonexistentConstructorName(..)
+        ))
+    )
+}
+
+#[test]
+fn error_nonexistent_name() {
     assert_matches!(
         transpile("import Prelude (nothing);"),
         Err(HuckError::NameResolution(NameError::NonexistentName(..)))
@@ -61,7 +69,7 @@ fn nonexistent_name() {
 }
 
 #[test]
-fn duplicate_binding() {
+fn error_duplicate_binding() {
     assert_matches!(
         transpile("a ++ a = 123;"),
         Err(HuckError::NameResolution(NameError::DuplicateBinding(..)))
@@ -69,13 +77,23 @@ fn duplicate_binding() {
 }
 
 #[test]
-fn duplicate_binding_lambda() {
+fn error_duplicate_binding_lambda() {
     assert_matches!(
         transpile(r#"foo = \a a -> 123;"#),
         Err(HuckError::NameResolution(
             NameError::DuplicateBindingLambda(..)
         ))
     )
+}
+
+#[test]
+fn name_in_scope() {
+    assert_matches!(transpile("foo = 123; bar = foo;"), Ok(_));
+}
+
+#[test]
+fn let_binding_in_scope() {
+    assert_matches!(transpile("foo = let bar = 123 in bar;"), Ok(_));
 }
 
 #[test]
@@ -95,14 +113,6 @@ fn clashes_type_constructors() {
 }
 
 #[test]
-fn clashes_import_redefinition() {
-    assert_matches!(
-        transpile("import Foo (foo); foo = 123;"),
-        Err(HuckError::NameResolution(NameError::ImportClash(..)))
-    )
-}
-
-#[test]
 fn clashes_import_redefinition_implicit_prelude() {
     assert_matches!(
         transpile("length 1 = 2;"),
@@ -114,6 +124,14 @@ fn clashes_import_redefinition_implicit_prelude() {
 fn clashes_import_type() {
     assert_matches!(
         transpile("import Foo (Data); type Data = MkData;"),
+        Err(HuckError::NameResolution(NameError::ImportClash(..)))
+    )
+}
+
+#[test]
+fn clashes_foreign_import() {
+    assert_matches!(
+        transpile(r#"foreign import "inspect" (inspect : forall a. a -> String); inspect = 123;"#),
         Err(HuckError::NameResolution(NameError::ImportClash(..)))
     )
 }
